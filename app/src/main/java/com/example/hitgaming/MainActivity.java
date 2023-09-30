4a353e770368eb28f1185328f608ec40c504987f
srjuchenko <srjuchenko@gmail.com>
2023-09-30 17:27:14 +0300

    add fragments

--- app/src/main/java/com/example/hitgaming/MainActivity.java
+++ app/src/main/java/com/example/hitgaming/MainActivity.java
@@ -1,14 +1,46 @@
 package com.example.hitgaming;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.hitgaming.fragments.HomeFragment;
+import com.example.hitgaming.fragments.ResultsFragment;
 
 public class MainActivity extends AppCompatActivity {
+    private Button showAllBtn;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Fragment newFragment = new ResultsFragment();
+        showAllBtn = findViewById(R.id.btn_showAll);
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+        Fragment homeFragment = new HomeFragment();
+        fragmentTransaction.add(R.id.frame_layout, homeFragment); // R.id.fragmentContainer is the ID of your container view
+
+        fragmentTransaction.commit();
+
+        showAllBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+                transaction.replace(R.id.frame_layout, newFragment);
+                transaction.addToBackStack(null);
+                transaction.commit();
+            }
+        });
+
     }
 }
